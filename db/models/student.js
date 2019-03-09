c080697d4a0a76a1da90f0c20ace5aab1b7cac50
Brandon Rowe <brandonhrowe@gmail.com>
2019-03-09 10:45:15 -0500

    cycle 1 complete

--- db/models/student.js
+++ db/models/student.js
@@ -3,6 +3,28 @@
 const Sequelize = require('sequelize');
 const db = require('../db');
 
-const Student = db.define();
+const Student = db.define("student", {
+  firstName: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  lastName: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+    email: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      isEmail: true
+    }
+  }
+});
+
+Student.beforeValidate(student => {
+  student.firstName = student.firstName.slice(0,1).toUpperCase() + student.firstName.slice(1);
+  student.lastName = student.lastName.slice(0,1).toUpperCase() + student.lastName.slice(1);
+
+})
 
 module.exports = Student;
