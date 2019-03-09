5562b6edbcb524f473f861ccf1ba472932aa7436
Brandon Rowe <brandonhrowe@gmail.com>
2019-03-09 11:36:02 -0500

    cycle2

--- routes/students.js
+++ routes/students.js
@@ -1,4 +1,14 @@
 const router = require('express').Router();
+const Student = require("../db/models/student");
 
+router.get("/students", async (req, res, next) => {
+  try {
+    let allStudents = await Student.findAll();
+    console.log(allStudents)
+    res.send(allStudents);
+  } catch (err) {
+    next(err);
+  }
+});
 
 module.exports = router;
