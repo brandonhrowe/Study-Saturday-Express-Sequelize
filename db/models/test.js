edc44bd9124fefd56ecc93734b9e40d566116b60
Brandon Rowe <brandonhrowe@gmail.com>
2019-03-09 15:41:17 -0500

    cycle 4 complete

--- db/models/test.js
+++ db/models/test.js
@@ -1,25 +1,25 @@
-'use strict';
-const Sequelize = require('sequelize');
-const db = require('../db');
-const Student = require('./student');
+"use strict";
+const Sequelize = require("sequelize");
+const db = require("../db");
+const Student = require("./student");
 
-const Test = db.define('test', {
+const Test = db.define("test", {
   subject: {
     type: Sequelize.STRING,
-    allowNull: false,
+    allowNull: false
     // validate: {
     //   id: /[\w]+/,
     // },
   },
   grade: {
     type: Sequelize.INTEGER,
-    allowNull: false,
+    allowNull: false
     // validate: {
     //   isNumeric: true,
     // },
-  },
+  }
 });
 
-Test.belongsTo(Student, { as: 'student' });
+Test.belongsTo(Student, { as: "student" });
 
 module.exports = Test;
--- routes/tests.js
+++ routes/tests.js
@@ -1,4 +1,47 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
+const Test = require("../db/models/test");
+const Student = require("../db/models/student")
+
+router.get("/", async (req, res, next) => {
+  try {
+    let tests = await Test.findAll();
+    res.send(tests);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.get("/:id", async (req, res, next) => {
+  try {
+    let id = await Number(req.params.id);
+    let test = await Test.findById(id);
+    res.send(test);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.post("/student/:studentId", async (req, res, next) => {
+  try {
+    let studId = await Number(req.params.studentId);
+    let newTestInfo = await req.body;
+    newTestInfo.studentId = studId;
+    let newTest = await Test.create(newTestInfo)
+    res.status(201).send(newTest)
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.delete("/:id", async (req, res, next) => {
+  try {
+    let testId = await Number(req.params.id);
+    await Test.destroy({where: {id: testId}});
+    res.sendStatus(204)
+  } catch (err) {
+    next(err);
+  }
+});
 
 module.exports = router;
