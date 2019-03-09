e7aad4a1d13eef8d0af8fd341d6669b82e16a4ac
Brandon Rowe <brandonhrowe@gmail.com>
2019-03-09 11:29:00 -0500

    cycle1

--- routes/students.js
+++ routes/students.js
@@ -1,6 +1,5 @@
 const router = require("express").Router();
 const Student = require("../db/models/student");
-const Test = require("../db/models/test");
 
 // router.get("/students", async (req, res, next) => {
 //   try {
