b166416c6cdc16db6d4e98ec1cb2f50d38816abe
Brandon Rowe <brandonhrowe@gmail.com>
2019-03-09 15:13:25 -0500

    student routes

--- routes/students.js
+++ routes/students.js
@@ -1,4 +1,73 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
+const Student = require("../db/models/student");
+
+router.get("/", async (req, res, next) => {
+  //don't have to go to "/students" because in app.js students is set up in "/students" directory
+  try {
+    let students = await Student.findAll();
+    res.send(students);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.get("/:id", async (req, res, next) => {
+  try {
+    let id = await Number(req.params.id);
+    let student = await Student.findById(id);
+    if (student) {
+      res.send(student);
+    } else {
+      res.sendStatus(404);
+    }
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.post("/", async (req, res, next) => {
+  try {
+    let newInfo = req.body;
+    let student = await Student.create(newInfo);
+    res.status(201).send(student);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.put("/:id", async (req, res, next) => {
+  try {
+    let studentId = await Number(req.params.id);
+    let newInfo = await req.body;
+    // student.update(newInfo).then(updatedStudent => {
+    //   res.send(updatedStudent);
+    // });
+    let updatedStudent = await Student.update(newInfo, {
+      where: { id: studentId },
+      returning: true,
+      plain: true
+    });
+    console.log(updatedStudent)
+    res.send(updatedStudent[1]);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.delete("/:id", async (req, res, next) => {
+  try {
+    // let id = await Number(req.params.id);
+    // let student = await Student.findById(id);
+    // student.destroy({ force: true }).then(() => {
+    //   res.sendStatus(204);
+    // });
+    let studentId = Number(req.params.id)
+    await Student.destroy({where: {id: studentId}})
+    res.sendStatus(204)
+  } catch (err) {
+    next(err);
+  }
+});
 
 module.exports = router;
--- routes/tests.js
+++ routes/tests.js
@@ -1,4 +1,36 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
+const Test = require("../db/models/test");
+
+router.get("/", async (req, res, next) => {
+  try {
+    let tests = await Test.findAll();
+    res.send(tests);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.get("/:id", async (req, res, next) => {
+  try {
+    let id = await Number(req.params.id);
+    let test = await Test.findById(id);
+    res.send(test);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.post("/student/:studentId", async (req, res, next) => {
+  try {
+    let studentId = await req.params.studentId;
+    let newTest = await req.body;
+    Test.findOrCreate(newTest).spread((newTest, created) => {
+      res.status(201).send(newTest);
+    });
+  } catch (err) {
+    next(err);
+  }
+});
 
 module.exports = router;
